4d1cb4904d4f5bf76261f596c447b66efdf9f282
Abdelmajid Elousse <a.elouss@paylik.ma>
2023-09-03 14:41:16 +0100

    Established connection to Database

--- Library-App/src/main/java/com/library/app/DbConnection.java
+++ Library-App/src/main/java/com/library/app/DbConnection.java
@@ -1,7 +1,22 @@
 package com.library.app;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbConnection {
-
+    public static Connection connect(){
+        String url = "jdbc:mysql://localhost:3306/mylibrary";
+        String user = "root";
+        String password = "";
+        Connection connection;
+        try {
+            // Create a connection to the database
+            connection = DriverManager.getConnection(url, user, password);
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
