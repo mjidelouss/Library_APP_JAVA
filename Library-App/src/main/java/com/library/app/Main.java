fe42f9156428b230ac4cc2c9fc5bee58dfd127bd
Abdelmajid Elousse <a.elouss@paylik.ma>
2023-09-07 12:16:20 +0100

    Working on a creative Menu

--- Library-App/src/main/java/com/library/app/Main.java
+++ Library-App/src/main/java/com/library/app/Main.java
@@ -5,23 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Connection dbConnection = DbConnection.connect();
-
+        menu();
         if (dbConnection != null) {
-            Book bookManager = new Book();
-            List<Book> books = bookManager.displayBooks(dbConnection, "Available");
-
-            for (Book book : books) {
-                System.out.println("Book ID: " + book.getId());
-                System.out.println("Title: " + book.getTitle());
-                System.out.println("Author: " + book.getAuthor());
-                System.out.println("ISBN: " + book.getIsbn());
-                System.out.println("Year: " + book.getYear());
-                System.out.println("Quantity: " + book.getQuantity());
-                System.out.println("Status: " + book.getStatus());
-                System.out.println();
-            }
+            librarianMenu();
             try {
                 dbConnection.close();
             } catch (SQLException e) {
@@ -32,44 +20,172 @@ public class Main {
         }
     }
 
-    public static int menu() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("---- Welcome to Library App Management System ----");
-        System.out.println("1- Login.");
-        System.out.println("2- Register.");
-        System.out.println("3- Quit.");
-        int choice = input.nextInt();
-        return choice;
+    public static int menu() throws InterruptedException {
+        String[] lines = art();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            for (String line : lines) {
+                System.out.println(line);
+                Thread.sleep(250); // Sleep for 500 milliseconds between lines
+            }
+            System.out.println('\t'+"                         ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██");
+            System.out.println('\t'+"                         ██╗                                                               ██");
+            System.out.println('\t'+"                         ██╗                     Welcome to YouCode's                      ██");
+            System.out.println('\t'+"                         ██╗                        Library System                         ██");
+            System.out.println('\t'+"                         ██╗                                                               ██");
+            System.out.println('\t'+"                         ██╗ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ █ ██ ██ ██ ██");
+            System.out.println('\t'+"                         ██╗                                                              ═██");
+            System.out.println('\t'+"                         ██╗ 1- Login.                                                    ═██");
+            System.out.println('\t'+"                         ██╗ 2- Register.                                                 ═██");
+            System.out.println('\t'+"                         ██╗ 3- Quit.                                                     ═██");
+            System.out.println('\t'+"                         ██╗                                                              ═██");
+            System.out.println('\t'+"                         ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    //
+
+                    break;
+                case 2:
+                    //
+
+                    break;
+                case 3:
+                    // Quit
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 
-    public int memberMenu() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Choose the operation you want to effect :");
-        System.out.println("1- Display the list of Available Books.");
-        System.out.println("2- Search for a Book.");
-        System.out.println("3- Borrow a Book.");
-        System.out.println("3- Return a Borrowed Book.");
-        System.out.println("4- Disconnect.");
-        System.out.println("5- Quit.");
-        int choice = input.nextInt();
-        return choice;
+    public int memberMenu() throws InterruptedException {
+        String[] lines = art();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            for (String line : lines) {
+                System.out.println(line);
+                Thread.sleep(250); // Sleep for 500 milliseconds between lines
+            }
+            System.out.println('\t'+"                         ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██");
+            System.out.println('\t'+"                         ██╗                                                               ██");
+            System.out.println('\t'+"                         ██╗                     Welcome to YouCode's                      ██");
+            System.out.println('\t'+"                         ██╗                        Library System                         ██");
+            System.out.println('\t'+"                         ██╗                                                               ██");
+            System.out.println('\t'+"                         ██╗ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ █ ██ ██ ██ ██");
+            System.out.println('\t'+"                         ██╗                                                              ═██");
+            System.out.println('\t'+"                         ██╗ 1- Display the list of Available Books.                      ═██");
+            System.out.println('\t'+"                         ██╗ 2- Search for a Book.                                        ═██");
+            System.out.println('\t'+"                         ██╗ 3- Borrow a Book.                                            ═██");
+            System.out.println('\t'+"                         ██╗ 4- Return a Borrowed Book.                                   ═██");
+            System.out.println('\t'+"                         ██╗ 5- Disconnect.                                               ═██");
+            System.out.println('\t'+"                         ██╗ 6- Quit.                                                     ═██");
+            System.out.println('\t'+"                         ██╗                                                              ═██");
+            System.out.println('\t'+"                         ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    //
+
+                    break;
+                case 2:
+                    //
+
+                    break;
+                case 3:
+                    //
+
+                    break;
+                case 4:
+                    //
+
+                    break;
+                case 5:
+                    //
+
+                    break;
+                case 6:
+                    // Quit
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 
-    public int librarianMenu() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Choose the operation you want to effect :");
-        System.out.println("1- Add a new Book.");
-        System.out.println("2- Modify an existing Book.");
-        System.out.println("3- Delete a Book.");
-        System.out.println("4- Display List of borrowed Books.");
-        System.out.println("5- Generate Books Statistiques report.");
-        System.out.println("6- Disconnect.");
-        System.out.println("7- Quit.");
-        int choice = input.nextInt();
-        return choice;
-    }
+    public static void librarianMenu() throws InterruptedException {
+        String[] lines = art();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            for (String line : lines) {
+                System.out.println(line);
+                Thread.sleep(250); // Sleep for 500 milliseconds between lines
+            }
+            System.out.println('\t'+"                         ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██");
+            System.out.println('\t'+"                         ██╗                                                               ██");
+            System.out.println('\t'+"                         ██╗                     Welcome to YouCode's                      ██");
+            System.out.println('\t'+"                         ██╗                        Library System                         ██");
+            System.out.println('\t'+"                         ██╗                                                               ██");
+            System.out.println('\t'+"                         ██╗ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ █ ██ ██ ██ ██");
+            System.out.println('\t'+"                         ██╗                                                              ═██");
+            System.out.println('\t'+"                         ██╗ 1- Add a new Book or a Copy.                                 ═██");
+            System.out.println('\t'+"                         ██╗ 2- Modify an existing Book.                                  ═██");
+            System.out.println('\t'+"                         ██╗ 3- Delete a Book.                                            ═██");
+            System.out.println('\t'+"                         ██╗ 4- Display List of borrowed Books.                           ═██");
+            System.out.println('\t'+"                         ██╗ 5- Generate Books Statistics report.                         ═██");
+            System.out.println('\t'+"                         ██╗ 6- Disconnect.                                               ═██");
+            System.out.println('\t'+"                         ██╗ 7- Quit.                                                     ═██");
+            System.out.println('\t'+"                         ██╗                                                              ═██");
+            System.out.println('\t'+"                         ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    // Add a new Book
 
-    public static void quitApp() {
-        System.exit(0);
+                    break;
+                case 2:
+                    // Modify an existing Book
+
+                    break;
+                case 3:
+                    // Delete a Book
+
+                    break;
+                case 4:
+                    // Display List of borrowed Books
+
+                    break;
+                case 5:
+                    // Generate Books Statistics report
+
+                    break;
+                case 6:
+                    // Disconnect
+
+                    break;
+                case 7:
+                    // Quit
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+    public static String[] art() {
+        String[] lines = {
+                '\t'+"      ██╗   ██╗ ██████╗ ██╗   ██╗ ██████╗ ██████╗ ██████╗ ███████╗    ██╗     ██╗██████╗ ██████╗  █████╗ ██████╗ ██╗   ██╗",
+                '\t'+"      ╚██╗ ██╔╝██╔═══██╗██║   ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝    ██║     ██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝",
+                '\t'+"       ╚████╔╝ ██║   ██║██║   ██║██║     ██║   ██║██║  ██║█████╗      ██║     ██║██████╔╝██████╔╝███████║██████╔╝ ╚████╔╝ ",
+                '\t'+"        ╚██╔╝  ██║   ██║██║   ██║██║     ██║   ██║██║  ██║██╔══╝      ██║     ██║██╔══██╗██╔══██╗██╔══██║██╔══██╗  ╚██╔╝  ",
+                '\t'+"         ██║   ╚██████╔╝╚██████╔╝╚██████╗╚██████╔╝██████╔╝███████╗    ███████╗██║██████╔╝██║  ██║██║  ██║██║  ██║   ██║   ",
+                '\t'+"         ╚═╝    ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝    ╚══════╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ",
+        };
+        return lines;
     }
 }
